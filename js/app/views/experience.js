8bdfc69fcaa26f8eda54d343c86962ddf7ca202a
Paul Sniezak <paulsniezak@gmail.com>
2015-11-27 00:49:44 +0100

    Debug sound processing

--- js/app/views/experience.js
+++ js/app/views/experience.js
@@ -1,8 +1,10 @@
-// Volume
-// Clear canvas ?
-// stress effect
-// Pulse
-// Gérer quand tu le redéposes direct
+// - Stress Effect
+// - Volume
+// - Clear canvas
+// - Quand on change de page, ça vire le son et absolument tout en fait
+// - Animation en hover sur les pastilles et éventuellement au temps (cf remplissage)
+// - Lowpass en changeant de tab
+// - design global
 
 var Experience = function(){
 
@@ -35,8 +37,6 @@ Experience.prototype.animateIn = function() {
 	});
 
   //Drag and drop
-
-  //see http://www.greensock.com/draggable/ for more details.
 
   var droppables = $(".element");
   var dropArea = $(".scene");
@@ -101,6 +101,8 @@ Experience.prototype.animateIn = function() {
   // SOUND PROCESSING
   var canvas = $('#canvas-spectrum')[0];
   var ctx = canvas.getContext('2d');
+  var ws = canvas.width;
+  var hs = canvas.height;
 
   var context = new AudioContext();
   var javascriptNode = context.createScriptProcessor(256, 1, 1);
@@ -190,13 +192,16 @@ function playSound(javascriptNode, source, analyser, buffer, url) {
 
 function queueSound(url) {
   window.setInterval(function() {
+    console.log(queue);
     for (var i = 0; i <= queue.length - 1; i++) {
+      console.log('plus'+queue);
       console.log(queue[i]);
       loadSound(queue[i]);
-      queue.pop();
     };
-    console.log('salut');
+    queue = [];
+    console.log('Time');
   }, mesure);
+  pusleEffect();
 }
 
 function initSpectrum(javascriptNode, analyser) {
@@ -204,7 +209,7 @@ function initSpectrum(javascriptNode, analyser) {
         var array =  new Uint8Array(analyser.frequencyBinCount);
         analyser.getByteFrequencyData(array);
 
-        ctx.clearRect(0, 0, 1296, 600);
+        ctx.clearRect(0, 0, ws, hs);
 
         drawSpectrum(array);
     }
@@ -212,7 +217,8 @@ function initSpectrum(javascriptNode, analyser) {
 
 function releaseSound(ctx, url) {
   sources['key' + url].disconnect();
-  ctx.clearRect(0, 0, 1296, 600);
+  delete sources['key' + url];
+  ctx.clearRect(0, 0, ws, hs);
 }
 
 function drawSpectrum(array) {
@@ -228,17 +234,229 @@ function drawSpectrum(array) {
       }
 };
 
+function pusleEffect() {
+  $('.scene--bouncer-circle').addClass( "scene--bouncer-outline-animated" );
+  $('.scene--bouncer-outline').addClass( "scene--bouncer-outline-animated" );
+  $('.scene--bouncer-outline-2').addClass( "scene--bouncer-outline-animated" );
+  $('.scene--bouncer-outline-3').addClass( "scene--bouncer-outline-animated" );
+}
+
+function secureSearch(buffer) {
+  if (sources['key' + buffer]) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 // Event dropped class changes
 $('.element').on('addDropped', function() {
   // $('.element.dropped').css('background-color', '#cfd3fc');
   // $('.element.dropped > span').css('color', '#fff');
     var buffer = $(this).data('src');
-    queue.push(buffer);
+    if (secureSearch(buffer)) {
+      console.log('Sample here');
+    } else {
+      console.log('Sample not here');
+      queue.push(buffer);
+      console.log(queue);
+    }
 });
 
 $('.element').on('removeDropped', function() {
   releaseSound(ctx, $(this).data('src'));
 });
+
+
+
+
+// $(function() {
+//   var W, H,
+//     canvas_particles, ctx, //ctx stands for context and is the "curso" of our canvas element.
+//     particleCount = 200,
+//     particles = []; //this is an array which will hold our particles Object/Class
+
+//   W = window.innerWidth;
+//   H = window.innerHeight;
+
+//   canvas_particles = $("#canvas-particles").get(0); //this "get(0) will pull the underlying non-jquery wrapped dom element from our selection
+//   canvas_particles.width = W;
+// canvas_particles
+//   ctx = canvas_particles.getContext("2d"); // settng the context to 2d rather than the 3d WEBGL
+//   ctx.globalCompositeOperation = "lighter";
+//   console.log(ctx);
+//   var mouse = {
+//     x: 0,
+//     y: 0,
+//     rx: 0,
+//     ry: 0,
+//     speed: 40,
+//     delta: 0
+//   };
+
+//   document.addEventListener('mousemove', function(e) {
+
+//     mouse.x = e.clientX || e.pageX;
+//     mouse.y = e.clientY || e.pageY;
+//     mouse.x -= W / 2;
+//     mouse.y -= H / 2;
+
+//   }, false);
+
+//   function randomNorm(mean, stdev) {
+
+//     return Math.abs(Math.round((Math.random() * 2 - 1) + (Math.random() * 2 - 1) + (Math.random() * 2 - 1)) * stdev) + mean;
+//   }
+
+//   //Setup particle class
+//   function Particle() {
+//       //using hsl is easier when we need particles with similar colors
+//       this.h = parseInt(0);
+//       this.s = parseInt(60 * Math.random() + 80);
+//       /*this.l=parseInt(30 * Math.random() + 60);*/
+//       this.l = parseInt(100);
+//       this.a = 1;
+
+//       this.color = "hsla(" + this.h + "," + this.s + "%," + this.l + "%," + (this.a) + ")";
+//       this.shadowcolor = "hsla(" + this.h + "," + this.s + "%," + this.l + "%," + parseFloat(this.a) + ")";
+
+//       this.x = Math.random() * W;
+//       this.y = Math.random() * H;
+
+//       this.direction = {
+//         "x": -1 + Math.random() * 2,
+//         "y": -1 + Math.random() * 2
+//       };
+//       //this.radius = 9 * ((Math.random()*2-1)+(Math.random()*2-1)+(Math.random()*2-1)+3);
+//       this.radius = randomNorm(0, 1);
+//       this.scale = 1;
+//       this.rotation = Math.PI / 4 * Math.random();
+
+//       this.grad = ctx.createRadialGradient(this.x, this.y, this.radius, this.x, this.y, 0);
+//       this.grad.addColorStop(0, this.color);
+//       this.grad.addColorStop(1, this.shadowcolor);
+
+//       this.vx = .2 * this.radius;
+//       this.vy = 3 * this.radius;
+
+//       this.valpha = 0.01 * Math.random() - 0.02;
+
+//       this.move = function() {
+//         this.x += this.vx * this.direction.x;
+//         this.y += this.vy * this.direction.y;
+//         this.rotation += this.valpha;
+//         //this.radius*= Math.abs((this.valpha*0.01+1));
+//       };
+
+//       this.changeDirection = function(axis) {
+//         this.direction[axis] *= -1;
+//         this.valpha *= -1;
+//       };
+//       this.draw = function() {
+//         ctx.save();
+//         ctx.translate(this.x + mouse.rx / -20 * this.radius, this.y + mouse.ry / -20 * this.radius);
+//         ctx.rotate(this.rotation);
+//         ctx.scale(1, this.scale);
+
+//         this.grad = ctx.createRadialGradient(0, 0, this.radius, 0, 0, 0);
+//         this.grad.addColorStop(1, this.color);
+//         this.grad.addColorStop(0, this.shadowcolor);
+
+//         ctx.beginPath();
+//         ctx.fillStyle = this.grad;
+//         ctx.arc(0, 0, this.radius, 0, Math.PI * 2, false);
+//         ctx.fill();
+//         ctx.restore();
+
+//       };
+
+//       this.boundaryCheck = function() {
+//         if (this.x >= W * 1.2) {
+//           this.x = W * 1.2;
+//           this.changeDirection("x");
+//         } else if (this.x <= -W * 0.2) {
+//           this.x = -W * 0.2;
+//           this.changeDirection("x");
+//         }
+//         if (this.y >= H * 1.2) {
+//           this.y = H * 1.2;
+//           this.changeDirection("y");
+//         } else if (this.y <= -H * 0.2) {
+//           this.y = -H * 0.2;
+//           this.changeDirection("y");
+//         }
+//       };
+//     } //end particle class
+
+//   function clearCanvas() {
+//       ctx.clearRect(0, 0, W, H);
+//     } //end clear canvas
+
+//   function createParticles() {
+//       for (var i = particleCount - 1; i >= 0; i--) {
+//         p = new Particle();
+//         particles.push(p);
+//       }
+//     } // end createParticles
+
+//   function drawParticles() {
+//       for (var i = particleCount - 1; i >= 0; i--) {
+//         p = particles[i];
+//         p.draw();
+//       }
+
+//     } //end drawParticles
+
+//   function updateParticles() {
+//       for (var i = particles.length - 1; i >= 0; i--) {
+//         p = particles[i];
+//         p.move();
+//         p.boundaryCheck();
+//       }
+//     } //end updateParticles
+
+//   function initParticleSystem() {
+//     createParticles();
+//     drawParticles();
+//   }
+
+//   function animateParticles() {
+//     clearCanvas();
+//     setDelta();
+//     update()
+//     drawParticles();
+//     updateParticles();
+//     requestAnimationFrame(animateParticles);
+//   }
+
+//   initParticleSystem();
+//   requestAnimationFrame(animateParticles);
+
+//   function setDelta() {
+//     this.now = (new Date()).getTime();
+//     mouse.delta = (this.now - this.then) / 1000;
+//     this.then = this.now;
+//   }
+
+//   function update() {
+
+//     if (isNaN(mouse.delta) || mouse.delta <= 0) {
+//       return;
+//     }
+
+//     var distX = mouse.x - (mouse.rx),
+//       distY = mouse.y - (mouse.ry);
+
+//     if (distX !== 0 && distY !== 0) {
+
+//       mouse.rx -= ((mouse.rx - mouse.x) / mouse.speed);
+//       mouse.ry -= ((mouse.ry - mouse.y) / mouse.speed);
+
+//     }
+
+//   };
+
+// });
 
 }; //end animateIn
 
