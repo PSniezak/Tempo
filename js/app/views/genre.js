1fe8daf1571e7d11578fc8bd1d2e3e01c0eae92e
Praszezinski William <praszezinskiw@gmail.com>
2015-11-26 20:40:45 +0100

    Version propre de la page genre

--- js/app/views/genre.js
+++ js/app/views/genre.js
@@ -24,38 +24,52 @@ Genre.prototype.animateIn = function() {
 		self.onAnimateIn();
 	});
 	
-		var intervDraw;
-		var ctx1 = document.getElementById('genre__canvas1').getContext('2d');
-		var ctx2 = document.getElementById('genre__canvas2').getContext('2d');
-		var ctx3 = document.getElementById('genre__canvas3').getContext('2d');
-		var ctx4 = document.getElementById('genre__canvas4').getContext('2d');
-		
-	draw(ctx1,"rgb(205,235,245)",'../../../img/Future.png');
-	draw(ctx2,"rgb(248,200,203)",'../../../img/Dubstep.png');
-	draw(ctx3,"rgb(209,211,246)",'../../../img/Deephouse.png');
-	draw(ctx4,"rgb(231,226,241)",'../../../img/Drumandbass.png');
+	//On déclare les variables pour les 4 canvas et les dessine une première fois
+	var ctx1 = function() {
+		draw(document.getElementById('genre__canvas1').getContext('2d'),"rgb(205,235,245)");
+	};
+
+	var ctx2 = function() {
+		draw(document.getElementById('genre__canvas2').getContext('2d'),"rgb(248,200,203)");
+	};
+
+	var ctx3 = function() {
+		draw(document.getElementById('genre__canvas3').getContext('2d'),"rgb(209,211,246)");
+	};
+
+	var ctx4 = function() {
+		draw(document.getElementById('genre__canvas4').getContext('2d'),"rgb(231,226,241)");
+	};
 	
+	ctx1();
+	ctx2();
+	ctx3();
+	ctx4();
 	
-	//animation en hover (refaire en switch ?)
+	//On déclare une variable pour arrêter le SetInterval quand il est lancé
+	var intervDraw;
+	
+	//Animation qui se lance quand on hover
 	$('.categories').mouseenter(function() {
 		var categoryChild = $(this).children("canvas");
 		if (categoryChild.attr('id') == "genre__canvas1")
-			intervDraw = setInterval(function() {draw(ctx1,"rgb(205,235,245)",'../../../img/Future.png')}, 100);
+			intervDraw = setInterval(ctx1, 100);
 		else if (categoryChild.attr('id') == "genre__canvas2")
-			intervDraw = setInterval(function() {draw(ctx2,"rgb(248,200,203)",'../../../img/Dubstep.png')}, 100);
+			intervDraw = setInterval(ctx2, 100);
 		else if (categoryChild.attr('id') == "genre__canvas3")
-			intervDraw = setInterval(function() {draw(ctx3,"rgb(209,211,246)",'../../../img/Deephouse.png')}, 100);
+			intervDraw = setInterval(ctx3, 100);
 		else if (categoryChild.attr('id') == "genre__canvas4")
-			intervDraw = setInterval(function() {draw(ctx4,"rgb(231,226,241)",'../../../img/Drumandbass.png')}, 100);
+			intervDraw = setInterval(ctx4, 100);
 		
 	});
 	
+	//On stop l'animation quand on quitte le div
 	$('.categories').mouseleave(function() {
 		clearInterval(intervDraw);
 	});
 	
-	//fonction qui dessine les rectangles
-	function draw(ctx, color, imgSrc) {
+	//Fonction qui dessine les rectangles et qu'on utilise pour dessiner nos canvas
+	function draw(ctx, color) {
 		  ctx.clearRect(0,0,420,140);
 		  ctx.strokeStyle = color;
 		  ctx.lineWidth = 7;
@@ -68,24 +82,20 @@ Genre.prototype.animateIn = function() {
 			
 		  for (var i=0; i<35 ; i++) {
 			  
-			  //ligne du haut
+			  //Dessine la ligne du haut
 			  ctx.beginPath();
            ctx.moveTo(50+i*9,65);
            ctx.lineTo(50+i*9,(Math.abs( Math.sin( counter ) )*40)+Math.floor(Math.random() * (15 - 1))+1);
 			  counter += increase;
            ctx.stroke();
 			  
-			  // ligne du bas
+			  //Dessine la ligne du bas
 			  ctx.beginPath();
            ctx.moveTo(50+i*9,75);
            ctx.lineTo(50+i*9,(Math.abs( Math.sin( counter2 ) )*40)+Math.floor(Math.random() * (15 - 1))+75);
 			  counter2 += increase2;
            ctx.stroke();
 			  }
-			  //image fond et texte : utiliser la fonction image de Canvas
-			    var img = new Image();
-				 img.src = imgSrc;
-				 ctx.drawImage(img,5,45);
 				 
 	}
 
